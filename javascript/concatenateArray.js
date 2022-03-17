3d9349b81ccbf3e2e765808a0fa35d633a236b80
Sungjun Choi <sungjun0110@gmail.com>
2022-03-16 20:44:03 -0700

    updates concatenateArray.js

--- javascript/concatenateArray.js
+++ javascript/concatenateArray.js
@@ -2,9 +2,13 @@
  * @param {number[]} nums
  * @return {number[]}
  */
- var getConcatenation = function(nums) {
-    return [...nums, ...nums];
+var getConcatenation = function(nums) {
+    const output = [];    
+    for (let i = 0; i < (nums.length) * 2; i++) {
+        output.push(nums[i % nums.length]);
+    }
+    return output;
 };
 
-console.log([1, 2, 3]);
+console.log(getConcatenation([1, 2, 3]));
 // -> [1, 2, 3, 1, 2, 3]
