c9dc450a17eaf3ed950d1d7b9fada646b48e7fc6
Sungjun Choi <sungjun0110@gmail.com>
2022-03-18 22:31:50 -0700

    add findFirstUniqueCharacter.js

---
+++ javascript/findFirstUniqueCharacter.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    let output = -1;
+    const dict = {};
+    for (let i = 0; i < s.length; i++) {
+        if (!dict[s[i]]) 
+            dict[s[i]] = [i];
+        else
+            dict[s[i]].push(i);
+    }
+    for (const [key, value] of Object.entries(dict)) 
+        if (value.length === 1) 
+            return value[0];
+    return output;
+};
